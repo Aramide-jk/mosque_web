b66837c118c91a0fa3bbf07fdad9bb45cf46128f
Aramide_jk <aramjay29@gmail.com>
2024-05-23 11:34:31 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,72 @@
-// Slider
-new Splide( '.splide' ,{
-  loop:true,
-  autoplay:true,
-}).mount();
+
+      
+// Dyas selection
+const optionContainer = document.getElementById("option-container");
+for (let i = 1; i <= 28; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.text = ` ${i}th`;
+  optionContainer.appendChild(option);
+}
+
+// country
+// Get the select element
+const countrySelect = document.getElementById("country-select");
+
+// Fetch the list of countries from the REST Countries API
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((countries) => {
+    // Loop through the countries and create an option element for each country
+    countries.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country.name.common;
+      option.text = country.name.common;
+      countrySelect.add(option);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching countries:", error);
+  });
+
+// keeping intouch
+function togglePhoneInput(radioButton) {
+  const phoneInputContainer = document.getElementById(
+    "phone-input-container",
+  );
+
+  if (radioButton.value === "yes") {
+    phoneInputContainer.classList.remove("hidden");
+  } else {
+    phoneInputContainer.classList.add("hidden");
+  }
+}
+
+
+// ////////////////////////////////////////////////
+const one_of=document.getElementById("one");
+const regular=document.getElementById("regular");
+const donation=document.getElementById("preffered_date");
+const payment=document.getElementById("payment_method","hidden");
+// const profile=getElementById("your_profile");
+const address=document.getElementById("your_address");
+const scroll_to_top=document.querySelector(".scroll_to_top");
+// const map=document.getElementById("map");
+
+
+
+one_of.addEventListener("click",()=>{
+  donation.classList.add("hidden")
+  payment.style.display="grid";
+  address.classList.add("hidden")
+
+})
+
+regular.addEventListener("click",()=>{
+  donation.classList.remove("hidden")
+  payment.style.display="none"
+  // address.classList.remove("hidden")
+  address.classList.remove("hidden")
+
+})
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}",
-          "./src/pages/donate/*.{html,js}"],
+  content: ["./src/*.{html,js}",],
+        //   "./src/pages/donate/*.{html,js}",
+        //   "./src/pages/complet_payment/*.{html,js}",
+        // "./src/pages/support_us/current_appeal/*.{html,js}",],
+          
   theme: {
     extend: {},
   },
