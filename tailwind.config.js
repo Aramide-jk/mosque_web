90b78aed0248f4e738f3136dfd89e5e613664edc
Aramide_jk <aramjay29@gmail.com>
2024-05-25 17:01:48 +0100

    folder restructuring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}",],
+  content: ["./*.{html,js}"],
         //   "./src/pages/donate/*.{html,js}",
         //   "./src/pages/complet_payment/*.{html,js}",
         // "./src/pages/support_us/current_appeal/*.{html,js}",],
